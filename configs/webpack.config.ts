0301e2e2d8047c31a6fcb1cae24758ecd95d8896
int0h <int0h@yandex.ru>
2020-03-19 16:21:38 +0100

    css support

--- configs/webpack.config.ts
+++ configs/webpack.config.ts
@@ -19,6 +19,22 @@ export const webpackConfig = (tsconfigPath: string): Configuration => ({
                 test: /\.jsx?$/,
                 use: ['source-map-loader'],
                 enforce: "pre"
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'fonts/'
+                        }
+                    }
+                ]
             }
         ],
     },
